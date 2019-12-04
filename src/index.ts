b9fc93818b60da0c1790dac102eed875ba413c71
Logan McAnsh <logan@mcan.sh>
2019-12-04 14:49:29 -0500

    feat: fallback to http if NODE_ENV or NOW_REGION are dev
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import { IncomingMessage } from 'http';
 
 function getBaseURL(req?: IncomingMessage) {
   if (req) {
-    const protocol = req.headers['x-forwarded-proto'] || 'https';
+    const isDev =
+      process.env.NODE_ENV === 'development' ||
+      process.env.NOW_REGION === 'dev1';
+
+    const protocol =
+      req.headers['x-forwarded-proto'] || isDev ? 'http' : 'https';
     const host = req.headers['x-forwarded-host'] || req.headers.host;
-
     return `${protocol}://${host}`;
+  } else {
+    return window.location.origin;
   }
-
-  return `${window.location.origin}`;
 }
 
 export { getBaseURL };
